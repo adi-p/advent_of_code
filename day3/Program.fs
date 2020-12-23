8bdce88771df4fe737488277f29fdd5320309bad
adi-p <adarvan.p@hotmail.com>
2020-12-23 16:14:01 -0500

    Day 3 - Part 2

--- day3/Program.fs
+++ day3/Program.fs
@@ -37,6 +37,17 @@ let main argv =
         |> File.ReadAllLines
         |> Array.map (fun str -> parseLine str)
 
-    let count = countTree (3,1) (0,0) landArray    
-    printfn "Tree count: %d" count
+    let steps = [ (1,1); (3,1); (5,1); (7,1); (1,2); ] 
+
+    let counts =
+        steps
+        |> List.map countTree
+        |> List.map (fun f -> f (0,0) landArray)   
+
+    for count in counts do
+        printfn "Tree count: %d" count
+
+    // mult gets too big - we need to use bigint!
+    let mult = counts |> List.fold (fun (acc:bigint) x -> acc*(bigint x))  (bigint 1)
+    printfn "Multiplication result: %A" mult 
     0 // return an integer exit code
