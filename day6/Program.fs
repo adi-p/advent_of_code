16eb0bd90a86efa7769123ff9c3b3539aeaf82b2
adi-p <adarvan.p@hotmail.com>
2020-12-27 13:19:39 -0500

    Day 6 - Part 2

--- day6/Program.fs
+++ day6/Program.fs
@@ -3,29 +3,42 @@
 open System
 open System.IO
 
-let parseQuestions lines =
-    let parseQuestionsHelper (acc, curMap) line =
+let parseQuestions_part1 lines =
+    let parseQuestionsHelper (acc, curSet) line =
         if line = "" then
-            (curMap::acc, Map.empty)
+            (curSet::acc, Set.empty)
         else
-            (acc, Seq.fold (fun m c -> Map.add c c m) curMap line)
+            (acc, Seq.fold (fun set c -> Set.add c set) curSet line)
 
     // this pattern again... I should look up if there is a better way of doing this
-    let (maps, lastMap) = List.fold parseQuestionsHelper ([], Map.empty) lines
-    (lastMap::maps)
+    let (sets, lastSet) = List.fold parseQuestionsHelper ([], Set.empty) lines
+    (lastSet::sets)
+
+let parseQuestions lines =
+    let parseQuestionsHelper (acc, curAcc) line =
+        if line = "" then
+            let curSet = Set.intersectMany curAcc //idk how efficient this is
+            (curSet::acc, [])
+        else
+            let newSet = Seq.fold (fun set c -> Set.add c set) Set.empty line
+            (acc, newSet::curAcc)
+
+    // this pattern again... I should look up if there is a better way of doing this
+    let (sets, lastList) = List.fold parseQuestionsHelper ([], []) lines
+    ((Set.intersectMany lastList)::sets)
 
 [<EntryPoint>]
 let main argv =
     let file = argv.[0]
-    let maps = 
+    let sets = 
         file
         |> File.ReadAllLines
         |> List.ofArray
         |> parseQuestions
 
-    // for map in maps do
-    //     printfn "%d" map.Count
+    // for set in sets do
+    //     printfn "%d" set.Count
 
-    printfn "total count: %d" (maps |> List.fold (fun sum m -> sum + m.Count) 0)
+    printfn "total count: %d" (sets |> List.fold (fun sum set -> sum + set.Count) 0)
 
     0 // return an integer exit code
