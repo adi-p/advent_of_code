0be842b1067a898620f0a91397af9e14ea5dd8f3
adi-p <adarvan.p@hotmail.com>
2021-01-16 17:22:14 -0500

    Day 8 - Part 2

--- day8/Program.fs
+++ day8/Program.fs
@@ -23,6 +23,38 @@ let runInstructions (instructions : Instruction array) =
 
     runHelper (0, 0, Set.empty)
 
+
+let runUncorrupt (instructions : Instruction array) =
+    let rec uncorruptHelper (acc, index, seenSet, isChangeMade) =
+        if index >= instructions.Length then
+            Some acc
+        else        
+            match Set.contains index seenSet with
+            | true -> None
+            | false -> 
+                let seenSet = Set.add index seenSet
+                // this is messy but w/e
+                (match instructions.[index] with
+                | Nop n ->
+                    if isChangeMade then
+                        uncorruptHelper (acc, index + 1, seenSet, true)
+                    else
+                        (match uncorruptHelper (acc, index + 1, seenSet, false) with
+                        | None -> uncorruptHelper (acc, index + n, seenSet, true)
+                        | Some i -> Some i)
+
+                | Jmp n -> 
+                    if isChangeMade then
+                        uncorruptHelper (acc, index + n, seenSet, true)
+                    else
+                        (match uncorruptHelper (acc, index + n, seenSet, false) with
+                        | None -> uncorruptHelper (acc, index + 1, seenSet, true)
+                        | Some i -> Some i)
+
+                | Acc n -> uncorruptHelper (acc + n, index + 1, seenSet, isChangeMade))
+
+    uncorruptHelper (0, 0, Set.empty, false)
+
 let parseLine (line : string) =
     let instruction = line.Split ' '
     let value = int instruction.[1] //this might not work
@@ -41,8 +73,13 @@ let main argv =
         |> Array.map parseLine
 
     let acc = runInstructions instructions
+    let acc2 = runUncorrupt instructions
 
     printfn "Acc: %d" acc
+
+    match acc2 with
+    | Some i -> printfn "uncorrupt acc: %d" i
+    | None -> printfn "couldn't uncorrupt the program"
     
 
 
