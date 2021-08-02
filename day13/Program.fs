c0a2c8f9e031c3ec62fee791e06114de3743e134
adi-p <adarvan.p@hotmail.com>
2021-08-02 14:04:15 -0400

    Incomplete Day 13 - Part 2

--- day13/Program.fs
+++ day13/Program.fs
@@ -12,10 +12,30 @@ let findBusTimestamp (minTimestamp : int) (buses: int seq) =
     |> Seq.map (fun bus -> (calcWaitTime bus, bus))
     |> Seq.minBy fst
 
+//TODO need to use chinese remainder theorem. THe numbers are too big to brute force.
+let findBusTimestamp2 (buses: (int*int) seq) =
+    let rec findStartTime startTime =
+        let isCorrect = 
+            buses
+            |> Seq.forall (fun (interval, bus) -> (startTime + interval) % bus = 0)
+        match isCorrect with 
+        | true -> startTime 
+        | false ->
+            let (_ ,firstBus) = buses |> Seq.head
+            findStartTime (startTime + firstBus)
+    findStartTime 0
+
 let parseLines (lines : (string array)) =
     let minTimestamp = int lines.[0]
     let buses = lines.[1].Split ',' |> Seq.choose (fun s -> match Int32.TryParse s with | (true,i) -> Some i | _ -> None) 
     (minTimestamp, buses)
+
+let parseLines2 (lines : (string array)) =
+    let buses = 
+        lines.[1].Split ',' 
+        |> Seq.mapi (fun index busString -> match Int32.TryParse busString with | (true, bus) -> Some (index, bus) | _ -> None) 
+        |> Seq.choose id
+    buses
 
 [<EntryPoint>]
 let main argv =
@@ -25,6 +45,12 @@ let main argv =
 
     let (minTimestamp, buses) = file |> File.ReadAllLines |> parseLines
     let (waitTime, bus) = findBusTimestamp minTimestamp buses
-
     printfn "Part 1: Bus: %d, wait time: %d, Result: %d" bus waitTime (waitTime * bus)
+
+
+    let buses2 = file |> File.ReadAllLines |> parseLines2
+    let smallestStartTime = findBusTimestamp2 buses2
+
+    printfn "Part 2: Bus: %d, min wait time: %d" bus smallestStartTime
+    
     0 // return an integer exit code
