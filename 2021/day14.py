0564a35099e08509da9f0cbd31b8fecff5a142bc
adi-p <adarvan.p@hotmail.com>
2021-12-22 10:52:44 -0500

    Day 14 - part 2

--- 2021/day14.py
+++ 2021/day14.py
@@ -1,24 +1,37 @@
 import sys
+import math
 
-from collections import Counter
+# Solution change from part 1 to part 2 was hinted to me by kade-robertson
 
+def get_counts(template, insertion_rules, steps):
+    def get_counts_helper(pair_counter, char_counter):
+        for i in range(steps):
+            new_pair_counter = {}
+            for pair, count in pair_counter.items():
+                a, b = pair
+                new_char = insertion_rules[a+b]
+                # count the new char
+                char_counter[new_char] = char_counter.get(new_char, 0) + count
 
-def get_counts(template, insertion_rules, count):
+                # count the pairs
+                new_pair_counter[(a + new_char)] = new_pair_counter.get((a + new_char), 0) + count 
+                new_pair_counter[(new_char + b)] = new_pair_counter.get((new_char + b), 0) + count
+            pair_counter = new_pair_counter
+        return char_counter
 
-    if count == 0:
-        return Counter(template)
-
+    # prep work for the real function
+    
+    new_pair_counter = {}
+    char_counter = {}
     pairs = zip(template, template[1:])
 
-    new_template = []
     for a, b in pairs:
-        new_template.append(a)
-        new_template.append(insertion_rules[a+b])
+        new_pair_counter[(a + b)] = new_pair_counter.get((a + b), 0) + 1
 
-
-    new_template.append(template[-1])
-    # print(new_template)
-    return get_counts(new_template, insertion_rules, count - 1)
+    for c in template:
+        char_counter[c] = char_counter.get(c, 0) + 1
+        
+    return get_counts_helper(new_pair_counter, char_counter)
 
 
 def parse_file(file):
@@ -48,10 +61,10 @@ def main():
     print("Part 1: result {}".format(max_count - min_count))
 
 
-    # counter = get_counts(list(template), insertion_rules, 40)
-    # min_count = min(map(lambda el: el[1], counter.items()))
-    # max_count = max(map(lambda el: el[1], counter.items()))
-    # print("Part 2: result {}".format(max_count - min_count))
+    counter = get_counts(list(template), insertion_rules, 40)
+    min_count = min(map(lambda el: el[1], counter.items()))
+    max_count = max(map(lambda el: el[1], counter.items()))
+    print("Part 2: result {}".format(max_count - min_count))
 
 
 if __name__ == "__main__":
